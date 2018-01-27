793c5f97d3ad74f3ffe4e7816b4818e1304c5507
Reece Martin <capt.rm.fae@gmail.com>
2018-01-26 21:48:38 -0500

    Added factorialCalculator function

--- myapp.js
+++ myapp.js
@@ -1,13 +1,6 @@
-var factApp = angular.module('factApp', []);
-
-// note: $scope is a service in angularjs, all services are annotated with a
-// "$" prefix. Also worth noting that passing the scope service here is a good
-// example of dependency injection, where we are passing scope in to our
-// controller.
-factApp.controller('factController', function($scope) {
-
-$scope.result = function(){
-
-    return $scope.number+1;
-};
-});
+angular.module('factApp', [])
+    .controller('factController', function($scope) {
+        $scope.result = function() {
+            return $scope.number;
+        };
+    });
