9b7c685929d2b627e6a13bd7bb452327f2d3b7e0
Reece Martin <capt.rm.fae@gmail.com>
2018-01-26 21:51:38 -0500

    Added factorialCalculator function

--- myapp.js
+++ myapp.js
@@ -1,6 +1,12 @@
 angular.module('factApp', [])
     .controller('factController', function($scope) {
-        $scope.result = function() {
-            return $scope.number;
+        $scope.factorial = function() {
+            var num = $scope.number;
+            var i = 1;
+            var prod = 1;
+            while(i <= num){
+                prod *= i;
+                i++;
+            }
         };
     });
