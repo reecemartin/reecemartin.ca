ae17275bc5c970f09a26b9becc867c600d3bc424
Reece Martin <capt.rm.fae@gmail.com>
2018-01-26 21:02:10 -0500

    Added comment to my js

--- myapp.js
+++ myapp.js
@@ -1,5 +1,9 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function() {
-    
+// note: $scope is a service in angularjs, all services are annotated with a
+// "$" prefix. Also worth noting that passing the scope service here is a good
+// example of dependency injection, where we are passing scope in to our
+// controller.
+myApp.controller('mainController', function($scope) {
+
 });
