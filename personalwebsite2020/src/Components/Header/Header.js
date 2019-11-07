21e768e7c4482527f049a3136203b766e394a3bb
Reece Martin <capt.rm.fae@gmail.com>
2019-11-07 00:06:07 -0500

    Adding new personal website files

---
+++ personalwebsite2020/src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Website-header">
+			{/* Photo */}
+			<header>
+				<>
+				{/* Further Subtext */}
+			</header>
+			{/* Links */}
+    </div>
+  );
+}
+
+export default App;
