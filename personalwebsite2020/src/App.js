6c91a15331492879b8b9451361a6d11c8d0369c4
Reece Martin <capt.rm.fae@gmail.com>
2019-11-16 23:24:04 -0500

    Adding drop shadow to the image component and some props for it

--- personalwebsite2020/src/App.js
+++ personalwebsite2020/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
 
       <Header/>
-      <Picture src={profile} height="100px" radius="border50" text=""/>
+      <Picture src={profile} height="100px" radius="border50" text="" imageShadowOffsetX="5px" imageShadowOffsetY="10px" imageShadowColor="blue"/>
       <Picture src={burjkhalifa2} width="100px" radius="border25"/>
       <Picture src={kendo} height="1000px" width="1000px"/>
 
--- personalwebsite2020/src/Components/Picture/Picture.js
+++ personalwebsite2020/src/Components/Picture/Picture.js
@@ -4,7 +4,7 @@ import './Picture.css'
 function Picture(props) {
     return (
       <div>
-        <img src={props.src} alt="Failed to Load" width={props.width} height={props.height} className={props.radius}/>
+        <img src={props.src} alt="Failed to Load" width={props.width} height={props.height} className={props.radius} style={{boxShadow:props.imageShadowOffsetX + " " + props.imageShadowOffsetY + " " + props.imageShadowColor}}/>
         <p>{props.text}</p>
       </div>
     );
