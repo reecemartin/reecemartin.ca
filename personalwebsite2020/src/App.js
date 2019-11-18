38e70c634da15aad2aec2a5b68cb6d2937ecd51a
Reece Martin <capt.rm.fae@gmail.com>
2019-11-18 09:25:00 -0500

    Removing standard Create React App Home

--- personalwebsite2020/src/App.js
+++ personalwebsite2020/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 // Images
-import logo from './logo.svg';
 import profile from './Images/Profile.jpg';
 import burjkhalifa2 from './Images/BurjKhalifa_2.jpeg';
 import kendo from './Images/Kendo.jpg';
@@ -19,20 +18,6 @@ function App() {
       <Picture src={burjkhalifa2} width="100px" radius="border25"/>
       <Picture src={kendo} height="1000px" width="1000px"/>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
