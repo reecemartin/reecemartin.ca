572db5feca44b23d50840992ae6716c701917e58
Reece Martin <capt.rm.fae@gmail.com>
2019-11-19 20:54:08 -0500

    Adding some buttons, other improvements

--- personalwebsite2020/src/Components/Header/Header.js
+++ personalwebsite2020/src/Components/Header/Header.js
@@ -2,9 +2,15 @@
 import React from "react";
 import "./Header.css";
 import Picture from "../Picture/Picture.js";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import red from '@material-ui/core/colors/red';
 
 // Resources
 import profile from "../../Images/Profile.jpg";
+
+const primary = red[500];
+const secondary = red[300];
 
 function Header() {
   return (
@@ -18,13 +24,19 @@ function Header() {
       />
       {/*Title*/}
       <header>
-        <h1>Reece Martin</h1>
-        <h3>
+        <h1 style={{fontFamily:"Roboto, sans-serif"}}>Reece Martin</h1>
+        <h3 style={{fontFamily:"Roboto, sans-serif"}}>
           Software Developer | Transit - Infrastructure Youtuber | Proud
           Torontonian
         </h3>
       </header>
       {/* Links */}
+      <ButtonGroup size="large" aria-label="small outlined button group" style={{paddingBottom:"20px"}}>
+        <Button color="primary">GitHub</Button>
+        <Button color="secondary">Resume</Button>
+        <Button color="primary">LinkedIn</Button>
+        <Button color="secondary">YouTube</Button>
+      </ButtonGroup>
     </div>
   );
 }
