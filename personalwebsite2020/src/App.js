96b7da851b1e6b59b9bfd85d289212a1e70d8c9f
Reece Martin <capt.rm.fae@gmail.com>
2019-11-26 22:06:02 -0500

    Adding some new copy

--- personalwebsite2020/src/App.js
+++ personalwebsite2020/src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Card title="Welcome to my life" body="Hi, my name is Reece" maxRelativeWidth="80%"/>
-      <Picture src={burjkhalifa2} maxRelativeWidth="20%" />
+      <Picture src={burjkhalifa2} maxRelativeWidth="30%" />
+      <Card title="Travel : Dubai" body="This picture was taken on my trip to Dubai in the United Arab Emirates. The building pictured is the 828m Burj Khalifa, the worlds tallest building as of 2019." maxRelativeWidth="60%"/>
       <Picture src={kendo} width="1000px" />
       <Footer />
     </div>
