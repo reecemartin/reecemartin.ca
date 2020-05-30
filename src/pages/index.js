7b1c091956b937f982aeec8e49ed44ab406721ee
Reece Martin <capt.rm.fae@gmail.com>
2020-05-29 23:16:41 -0700

    Added profile image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { css } from "@emotion/core"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import "./index.css";
 
 export default function Home() {
@@ -8,18 +8,33 @@ export default function Home() {
     <div
       class="container"
     >
-      <h1>Reece Martin's Website</h1>
-
-      <Link 
-        to="/posts"
+      <div
         css={css`
-                color: inherit;
-              `}
-        >
-        <h3>My Blog Posts</h3>
-      </Link>
-
-      
+          height: 20%;
+        `}
+      >
+      </div>
+      <div
+        css={css`
+          height: 60%;
+        `}
+      >
+        <img 
+          src={require("../../res/images/profile.jpg")}
+          css={css`
+            height: 75%;
+            border-radius: 50%;
+            margin: auto;
+            display: block;
+            border: 5px solid white;
+          `}
+        />
+      </div>
+      <div
+        css={css`
+          height: 20%;
+        `}
+      ></div>
     </div>
   )
 }
