ecb3c5a22225412dc51d983f5b81a0b5ce6d0790
Reece Martin <capt.rm.fae@gmail.com>
2020-07-02 20:12:40 -0700

    Updated youtube page

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -4,24 +4,26 @@ import { css } from "@emotion/core"
 import Header from "../components/header";
 
 export default (props) => (
-  <div 
+  <div
     className={props.className}
-    style={{height: "100%", width: "100%"}}
+    style={{ height: "100%", width: "100%" }}
     css={props.css}
   >
     <Helmet key="app-head">
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <title>reece martin.</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-  </Helmet>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <title>reece martin.</title>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <script src="https://apis.google.com/js/platform.js"></script>
+      <script src="https://apis.google.com/js/api.js"></script>
+    </Helmet>
 
-  {
-    props.noHeader ? <></> : <Header/>
-  }
-  {props.children}
+    {
+      props.noHeader ? <></> : <Header />
+    }
+    {props.children}
   </div>
 );
--- src/pages/youtube.js
+++ src/pages/youtube.js
@@ -1,15 +1,178 @@
 import React from "react"
 import { css } from "@emotion/core"
+import Grid from "@material-ui/core/Grid"
 
 import Layout from "../layouts/layout";
 
+const breakpoints = [576, 768, 992, 1200]
+const mq = breakpoints.map(bp => `@media (min-width: ${bp}px)`)
+
 export default function YouTube() {
   return (
-    <Layout
+    <Layout css={css`
+      ${mq[2]} {
+        overflow:hidden;
+      }
+      
+      `}
     >
-      <h1>
-        This is the youtube page
-      </h1>
+      <Grid
+        container
+        css={css`
+          height: 100%;
+          width: 100%; 
+          ${mq[1]} {
+            height: 88vh; 
+            padding-bottom: 0;
+          }
+          `}
+      >
+        <Grid
+          xs={12}
+          md={6}
+          container
+          item
+          justify="center"
+          alignItems="center"
+          css={css`
+          background-color: #8c8c8c;
+          text-align: center;
+          padding-bottom: 100px;
+        `}
+        >
+          <Grid
+            item
+            css={css`
+              height: 65%;
+              width: 80%;
+            `}
+          >
+            <h2
+              css={css`
+                background-color: #333333;
+                color: white;
+                padding: 10px;
+              `}
+            >YouTube</h2>
+            <a
+              href="https://www.youtube.com/channel/UCf4iKXL_SJQ5d0qsKkboRRQ"
+              target="_blank"
+              rel="noopener noreferrer"
+              css={css`
+                color: white;
+                text-decoration: none;
+                transition: 0.3s;
+
+                :hover {
+                  color: gray;
+                }
+              `}
+            >
+              <h1
+                css={css`
+                background-color: #333333;
+                padding: 10px;
+                font-size: 300%;
+              `}
+              >
+                RMTransit
+              </h1>
+            </a>
+            <div
+              css={css`
+              background-color: lightgray;
+              padding: 10px;
+            `}
+            >
+              <div
+                class="g-ytsubscribe"
+                data-channelid="UCf4iKXL_SJQ5d0qsKkboRRQ"
+                data-layout="full"
+                data-count="default"
+
+              ></div>
+            </div>
+            <div
+              css={css`
+                color: white;
+                margin-top: 5%;
+              `}
+            >
+              <div
+                css={css`
+                  display: inline-block;
+                  width: 45%;
+                  margin-right: 10%;
+                  background-color: #333333;
+                  padding: 10px;
+                  font-size: 80%;
+                `}
+              >
+                Subscribers: 7706
+              </div>
+              <div
+                css={css`
+                  display: inline-block;
+                  width: 45%;
+                  background-color: #333333;
+                  padding: 10px;
+                  font-size: 80%;
+                `}
+              >
+                Total Views: 1,163,117
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid
+          xs={12}
+          md={6}
+          container
+          item
+          justify="center"
+          alignItems="center"
+          css={css`
+          
+        `}
+        >
+          <Grid
+            item
+            css={css`
+              height: 80%;
+              width: 80%;
+              background-color: lightgray;
+              border-radius:5px;
+              padding: 20px;
+            `}
+          >
+            <p
+              css={css`
+                color: black;
+                text-decoration: none;
+              `}
+            >
+              One of my main passions right now is my YouTube channel RMTransit. Focused on exploring transit and infrastructure topics and issues, I have been running this channel with my partner Ellen for around 4 years.
+            </p>
+            <p
+              css={css`
+              color: black;
+              text-decoration: none;
+              a {
+                color: black;
+              }
+            `}
+            >
+              Some of our most popular YouTube videos include <a href="https://www.youtube.com/watch?v=ttek78mRoRg" target="_blank"
+                rel="noopener noreferrer">this one</a> we made on the O-Train Confederation Line's opening, <a href="https://www.youtube.com/watch?v=TXEO7DbXPS0" target="_blank"
+                  rel="noopener noreferrer">this overview</a> of London's new Crossrail project, and <a href="https://www.youtube.com/watch?v=ILqWCzaZECs" target="_blank"
+                    rel="noopener noreferrer">this overview</a> of the future of Montreal's rapid transit. We also make transit station tours like <a href="https://www.youtube.com/watch?v=5RqGSiW7vRc" target="_blank"
+                      rel="noopener noreferrer">this one</a>, news story analysis videos like <a href="https://www.youtube.com/watch?v=VA_02xHu4j0" target="_blank"
+                        rel="noopener noreferrer">this one</a>, and opinions on different topics like <a href="https://www.youtube.com/watch?v=60OjiUgZuPM" target="_blank"
+                          rel="noopener noreferrer">this one</a>.
+            </p>
+          </Grid>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
