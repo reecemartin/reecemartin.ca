25bcd245dbe8e8923681dd1b05ac1a0740c7dc9b
Ellen (Yufei) Chen <chenellen007@gmail.com>
2020-07-03 02:52:17 -0700

    Beautified contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,23 @@
 import React from "react"
 import { css } from "@emotion/core"
 import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 import Layout from "../layouts/layout";
+
+const breakpoints = [576, 768, 992, 1200]
+const mq = breakpoints.map(bp => `@media (min-width: ${bp}px)`)
+
+function FormLabel (props){
+  return (
+    <p
+      css={props.css}
+    >
+      {props.children}
+    </p>
+  )
+}
 
 export default function Contact() {
   return (
@@ -10,44 +25,53 @@ export default function Contact() {
     >
       <h1
         css={css`
-          padding: 20px 0;
+          padding-top: 20px;
           text-align: center;
+          font-size: 300%;
         `}
       >
         Get in Touch!
       </h1>
 
-      <Grid container>
+      <Grid 
+        container
+        css={css`
+          padding: 0 10%;
+        `}
+      >
         <Grid 
           item 
           xs={12} 
           md={6}
           align-items="center"
+          css={css`
+            padding: 10px 50px;
+          `}
           >
         <form name="Contact Form" method="POST" data-netlify="true" action="/thank-you" >
           <input type="hidden" name="form-name" value="Contact Form" />
           <div 
             css={css`
               text-align: center;
+              .input-field {
+                width: 100%;
+                margin: 10px;
+              }
             `}
           >
-            <label>Your Email:</label>
-            <input type="email" name="email" />
-          </div>
-          <div
-            css={css`
-            text-align: center;
-            
-          `}
-          >
-            <label>Message:</label>
-            <textarea name="message" />
-            <button
-              css={css`
-                display: block;
-                
-              `}
-              type="submit">Send</button>
+            <TextField className="input-field" id="input-name" name="name" label="Name" variant="filled" />
+            <TextField className="input-field" id="input-email" name="email" label="Email" variant="filled" />
+            <TextField
+              className="input-field"
+              id="input-message"
+              name="message"
+              label="Message"
+              rows="4"
+              variant="filled"
+              multiline
+              rowsMax={4}
+            />
+            <Button className="input-field" type="submit" variant="outlined">Send</Button>
           </div>
           
         </form>
