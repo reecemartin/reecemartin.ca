0db51c08610d94501d865662ff7e61c3212e59d4
Reece Martin <capt.rm.fae@gmail.com>
2020-09-04 20:00:12 -0700

    Updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Button from "@material-ui/core/Button"
+import Grid from "@material-ui/core/Grid"
 import { css } from "@emotion/core"
 
 import Layout from "../layouts/layout";
@@ -18,112 +19,108 @@ export default function About() {
       <div
         css={css`
           display: block;
-          ${mq[2]} {
-            display: flex;
-            flex-wrap: wrap;
-            align-items: flex-start;
-          }
+          width: 95%;
+          min-height: 750px;
+          margin: 2.5% auto;
+          border: 1px solid grey;
+          border-radius: 5px;
         `}
       >
-        <h1
+        <Grid
+          container
           css={css`
-            text-align: center;
-            font-size: 450%;
-            width: 100vw;
-            letter-spacing: 5px;
-            margin-bottom: 0;
-            margin: auto;
-            ${mq[1]} {
-              font-size: 700%;
-            }
-            ${mq[2]} {
-              font-size: 800%;
-              flex: 0 0 50%;
-            }
+            width: 100%;
+            height: 100%;
           `}
         >
-          ABOUT
-        </h1>
-        <h1
-          css={css`
-            text-align: center;
-            font-size: 800%;
-            width: 100vw;
-            letter-spacing: 5px;
-            margin: auto;
-            ${mq[2]} {
-              flex: 1;
-            }
-          `}
-        >
-          ME
-        </h1>
-        <div
-          css={css`
-            width: 90%;
-            margin: auto;
-            margin-top: 0;
-            margin-bottom: 50px;
-            min-height: 100px;
-            background-color: lightgray;
-            padding: 20px;
-            ${mq[2]} {
-              flex: 0 0 45%;
-              margin: 50px;
-            }
-          `}
-        >
-          <p
+          <Grid
+            item
             css={css`
-              width: 100%;
-              height: 100%;
-              font-size: 120%;
-              text-align: justify;
-              margin-bottom: 0;
-              ${mq[2]} {
-                font-size: 100%;
-              }
+              // background-color: blue;
             `}
+            xs={12}
+            md={6}
           >
-            Hi, my name's Reece. I'm a passionate software developer and computer scientist who graduated from the University of Toronto with a Honours Bachelor of Science in Computer Science in 2020.
-
-I've substantial industry experience particularly working in the Financial Sector in companies both small and large. During these experiences I've done a wide range or work from Backend Java Development, to Frontend ReactJS Development, more important than that - I have a love of learning and self development that drives everything I do.
-          </p>
-        </div>
-        <div
-          css={css`
-            width: 90%;
-            margin: auto;
-            margin-top: 0;
-            margin-bottom: 50px;
-            min-height: 100px;
-            background-color: #606060;
-            padding: 20px;
-            ${mq[2]} {
-              flex: 1;
-              margin: 50px;
-            }
-          `}
-        >
-          <p
+            <div
+              css={css`
+                background-color: white;
+                width: 50%;
+                margin: auto;
+                margin-top: 15%;
+                height: 65%;
+                text-align: center;
+              `}
+            >
+              <img
+                src={require("../../res/images/about-pic.jpg")}
+                css={css`
+                  display: block;
+                  height: auto;
+                  width: 90%;
+                  margin: auto;
+                  margin-bottom: 0;
+                `}
+              >
+              </img>
+              <h2
+                css={css`
+                  font-size: 300%;
+                  margin-top: 2rem;
+                `}
+              >
+                about me.
+              </h2>
+            </div>
+          </Grid>
+          <Grid
+            item
             css={css`
-              width: 100%;
-              height: 100%;
-              font-size: 120%;
-              text-align: justify;
-              margin-bottom: 0;
-              color: white;
-              ${mq[2]} {
-                font-size: 100%;
-              }
+              // background-color: yellow;
             `}
+            xs={12}
+            md={6}
           >
-            Outside of Software Development I have previously run a YouTube Channel with over 9000 subscribers and over 1 million views focused on communicating Public Transit Projects and Plans in a simplified and digestible format. I also create videos talking about project construction, news, vehicle design and service planning.
+            <div
+              css={css`
+                min-height: 80%;
+                width: 80%;
+                margin: auto;
+                margin-top: 10%;
+                border: 1px solid black;
+                border-radius: 5px;
+                padding: 10px;
+                text-align: center;
+              `}
+            >
+              <p>
+              Hi, my name's Reece. I'm a passionate software developer and computer scientist who graduated from the University of Toronto with a Honours Bachelor of Science in Computer Science in 2020.
+              </p>
 
-            Finally, on a final note I have a great passion for teaching and education which I have engaged both through the educational content I create for YouTube and through previous positions such as working as a Computer Science Teaching Assistant. I find teaching very rewarding and I hope to continue with it in the future. 
-          </p>
-        </div>
+              <p>
+              I've substantial industry experience particularly working in the Financial Sector in companies both small and large. During these experiences I've done a wide range or work from Backend Java Development, to Frontend ReactJS Development, more important than that - I have a love of learning and self development that drives everything I do.
+              </p>
+              
+              <p>
+              Outside of Software Development I have previously run a YouTube Channel with over 9000 subscribers and over 1 million views focused on communicating Public Transit Projects and Plans in a simplified and digestible format. I also create videos talking about project construction, news, vehicle design and service planning.
+              </p>
+              
+              <p>
+              And on a final note, I have a great passion for teaching and education which I have engaged both through the educational content I create for YouTube and through previous positions such as working as a Computer Science Teaching Assistant. I find teaching very rewarding and I hope to continue with it in the future. 
+              </p>
+            </div>
+          </Grid>
+        </Grid>
       </div>
+      {/* <div
+        css={css`
+          display: block;
+          width: 100%;
+          height: 750px;
+          background-color: blue;
+        `}
+      >
+        some places i've been to
+      </div> */}
     </Layout>
   )
 }
